bbfcd569f78cca9a008904a24b1780f3aa683492
Ignacio-Perez-2023 <ignacio.lopez@kellogg.northwestern.edu>
2023-01-10 22:23:25 +0000

    SQL exercises (week 2 in class)

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,4 +1,9 @@
 -- How many lifetime hits does Barry Bonds have?
+-- Note from IP: 
+
+SELECT sum(stats.hits) FROM stats 
+INNER JOIN players ON stats.player_id = players.id
+ WHERE first_name = "Barry" AND last_name = "Bonds";
 
 -- Expected result:
 -- 2935
